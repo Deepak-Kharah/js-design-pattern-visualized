498d94474bc67a0b4891dabae49e4e4a531abb25
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-04-12 23:56:05 +0530

    feat: add  background pattern

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Paragraph from "./Paragraph";
 import { H1 } from "./Heading";
+import style from "./title-section.module.css";
+import classNames from "classnames";
 
 interface TitleSectionProps {
   body: React.ReactNode;
@@ -9,7 +11,12 @@ interface TitleSectionProps {
 
 function TitleSection({ title, body }: TitleSectionProps) {
   return (
-    <section className="px-7 py-10 flex flex-col justify-center items-center gap-10 sm:gap-16 min-h-svh">
+    <section
+      className={classNames(
+        style["title-section"],
+        "px-7 py-10 flex flex-col justify-center items-center gap-10 sm:gap-16 min-h-svh"
+      )}
+    >
       <H1>{title}</H1>
       <Paragraph>{body}</Paragraph>
     </section>
